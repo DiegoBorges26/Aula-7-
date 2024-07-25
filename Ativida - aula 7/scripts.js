97753269395b22c0b568de05b4ac148601860500
DiegoBorges26 <diego.fort2011@hotmail.com>
2024-07-25 12:14:08 -0300

    Add files via upload

---
+++ Ativida - aula 7/scripts.js
@@ -0,0 +1,106 @@
+//EXEMPLEO ARROW FUNCTIONS
+// Função tradiconal
+// function soma(a, b){
+//    return a + b;
+// }
+
+
+// const somaArrow = (a, b) => a + b
+
+// console.log(soma(2, 3))
+// console.log(somaArrow(2,3));
+
+//--------------------------------------------
+
+//EXEMPLEO FUNÇÕES CALLBACK
+
+    // function somarNumeros(a, b) {
+    //     return a + b;
+    // }
+    // const soma = somarNumeros(10,1);
+
+    // function  imprmimirResultado(resultado){
+    //     console.log(`o resultado da soma é ${resultado}`)
+    // }
+    // imprimirResultado(soma)
+
+//-------------------------------------------------
+
+//EXEMPLEO FUNÇÕES CALLBACK
+
+// function saudacao(nome) {
+//     alert(`Olá ${nome}`);
+// }
+
+// function chamadaCallback(callback){
+//     let nome = prompt('Por favor insira seu nome');
+//     callback(nome)
+// }
+
+// chamadaCallback(saudacao)
+
+//---------------------------------------------------
+
+//Manipulação de Números:
+
+//const numero = parseInt("3.14");
+//console.log(numero);
+
+//const decimal = parseFloat(42);
+//console.log(decimal);
+
+//const pi = Math.PI;
+//console.log(pi.toFixed(2));
+
+//---------------------------------------------------
+
+
+//ATIVIDADE 1
+
+// function multiplicar(a, b){
+//    return a * b;
+// }
+
+
+// const multiplicarArrow = (a, b) => a * b
+
+// console.log(multiplicar(2, 3))
+// console.log(multiplicarArrow(2,3));
+
+
+//----------------------------------------------------
+
+//ATIVIDADE 2 - Não conseguir fazer
+
+// function  contarVogais()
+
+
+
+
+//-------------------------------------------------------
+
+//ATIVIDADE 3 - Tive ajuda de um colega do lado
+
+// function converterTemperatura(temperatura){
+
+//     // let temperatura = prompt('Digite a temperatura em celsius')
+//     let resultado = temperatura * 9/5 + 32
+//     console.log(resultado)
+// }
+
+
+
+
+// function chamarFunção(){
+//     let temperatura = prompt('Digite a temperatura em celsius')
+//     converterTemperatura(temperatura)
+
+// }
+// chamarFunção()
+
+//-------------------------------------------------------------
+
+
+//ATIVIDAE 4 - Não deu tempo
+
+// function receberNumero()
